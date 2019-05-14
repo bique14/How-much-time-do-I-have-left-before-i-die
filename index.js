f0356071774f69bf2af7971ac533d488d6ced867
Peerasorn Hemsart <peerasorn@Peerasorns-MacBook-Pro.local>
2019-05-14 19:25:19 +0700

    Update thesis presentation

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const posterEnd = moment('Thu May 08 2019 08:00:00 GMT+0700')
 const finalEnd = moment('Thu May 30 2019 13:00:00 GMT+0700')
+const thesisEnd = moment('Thu May 23 2019 16:00:00 GMT+0700')
 
 const x = setInterval(function () {
   const now = moment(new Date())
 
   const posterRemain = moment.duration(posterEnd.diff(now))
   const finalRemain = moment.duration(finalEnd.diff(now))
+  const thesisRemain = moment.duration(thesisEnd.diff(now))
 
   const posterDays = pad(+posterRemain.days())
   const posterHours = pad(+posterRemain.hours())
@@ -17,8 +19,14 @@ const x = setInterval(function () {
   const finalMinutes = pad(+finalRemain.minutes())
   const finalSeconds = pad(+finalRemain.seconds())
 
+  const thesisDays = pad(+thesisRemain.days())
+  const thesisHours = pad(+thesisRemain.hours())
+  const thesisMinutes = pad(+thesisRemain.minutes())
+  const thesisSeconds = pad(+thesisRemain.seconds())
+
   document.getElementById('count-poster').innerHTML = `${posterDays} ${posterHours} ${posterMinutes} ${posterSeconds}`
   document.getElementById('count-presentation').innerHTML = `${finalDays} ${finalHours} ${finalMinutes} ${finalSeconds}`
+  document.getElementById('count-thesis').innerHTML = `${thesisDays} ${thesisHours} ${thesisMinutes} ${thesisSeconds}`
 
   // 1 day = 86,400,000 millisecond
   const warningDays = 86400000 * 3
@@ -34,6 +42,11 @@ const x = setInterval(function () {
   if (posterRemain._milliseconds < 0) {
     document.getElementById('count-poster').innerHTML = 'EXPIRED'
     document.getElementById('count-poster').style.color = 'red'
+    document.getElementById('date-format').innerHTML = ''
+  }
+  if (thesisRemain._milliseconds < 0) {
+    document.getElementById('count-thesis').innerHTML = 'EXPIRED'
+    document.getElementById('count-thesis').style.color = 'red'
     document.getElementById('date-format').innerHTML = ''
   }
   if (finalRemain._milliseconds < 0) {
